a1bdfb60c071d04a95874bf51ed09ae5b0c9693b
Petar Popovic <petarpnp@gmail.com>
2022-10-28 21:30:48 +0200

    Add og title and description

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
       <Head>
         <title>NFT Belgrade</title>
         <meta name="description" content="The biggest NFT Conference in the CEE region" />
+
+
+        <meta property="og:title" content="NFT Belgrade"/>
+        <meta property="og:description" content="The biggest NFT Conference in the CEE region"/>
         <meta property="og:image" content="/nft-blegrade-og.jpg"/>
 
         <meta name="twitter:card" content="summary" />
