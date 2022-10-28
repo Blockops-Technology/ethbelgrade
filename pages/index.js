90d13657089f04df5eb59e5c7841e4a3372d4390
Petar Popovic <petarpnp@gmail.com>
2022-10-28 21:54:25 +0200

    Add full link for og image

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,11 @@ export default function Home() {
 
         <meta property="og:title" content="NFT Belgrade"/>
         <meta property="og:description" content="The biggest NFT Conference in the CEE region"/>
-        <meta property="og:image" content="/nft-blegrade-og.jpg"/>
+        <meta property="og:image" content="https://nftbelgrade.com/nft-blegrade-og.jpg"/>
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@nftbelgrade" />
+        <meta property="og:image" content="/nft-blegrade-og.jpg"/>
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
