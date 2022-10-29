7746ca99b0b6023457418a8ff77f25fcf45a86c6
Petar Popovic <petarpnp@gmail.com>
2022-10-29 12:14:33 +0200

    Add twitter image

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@nftbelgrade" />
-        <meta property="og:image" content="/nft-blegrade-og.jpg"/>
+        <meta name="twitter:image" content="https://nftbelgrade.com/nft-blegrade-og.jpg" />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
