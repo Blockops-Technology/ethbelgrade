9b806b4e52076aeefab903823b905741a55152b3
Petar Popovic <petarpnp@gmail.com>
2022-10-29 10:03:07 +0200

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from "next/script";
+
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-NW9VQBBQ61"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}  
+            gtag('js', new Date());
+            gtag('config', 'G-NW9VQBBQ61');
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
