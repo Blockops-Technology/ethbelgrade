457548370274512c35d25017a336fb1d177c6db6
Petar Popovic <petarpnp@gmail.com>
2022-11-06 14:08:56 +0000

    Change words NFT to ETH

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>NFT Belgrade</title>
-        <meta name="description" content="The biggest NFT Conference & NFT Gaming Hackathon in the CEE region" />
+        <title>ETH Belgrade</title>
+        <meta name="description" content="The biggest ETH Conference in the CEE region" />
 
 
-        <meta property="og:title" content="NFT Belgrade"/>
-        <meta property="og:description" content="The biggest NFT Conference & NFT Gaming Hackathon in the CEE region"/>
+        <meta property="og:title" content="ETH Belgrade"/>
+        <meta property="og:description" content="The biggest ETH Conference in the CEE region"/>
         <meta property="og:image" content="https://nftbelgrade.com/nft-blegrade-og.jpg"/>
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="NFT Belgrade" />
-        <meta name="twitter:description" content="The biggest NFT Conference & NFT Gaming Hackathon in the CEE region"/>
+        <meta name="twitter:title" content="ETH Belgrade" />
+        <meta name="twitter:description" content="The biggest ETH Conference in the CEE region"/>
         <meta property="og:type" content="website" />
         <meta name="twitter:site" content="@nftbelgrade" />
         <meta name="twitter:image" content="https://nftbelgrade.com/nft-blegrade-og.jpg" />
@@ -25,8 +25,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.content}>
-          <img src="/NFTBelgrade-text-logo.svg" alt="NFT Belgrade conference logotype" />
-          <p className={styles.date}>June, 2023</p>
+          <img src="/NFTBelgrade-text-logo.svg" alt="ETH Belgrade conference logotype" />
+          <p className={styles.date}>5-6th June, 2023</p>
           <div className={styles.buttons}>
             <a className={styles.button} href="https://xntkpyvzukn.typeform.com/to/ohkYPQyW" target="_blank" rel="noreferrer noopener">Get whitelisted</a>
             <a className={styles.ghostButton} href="https://t.me/+MIGPL5WcN_w2YWQ8" target="_blank" rel="noreferrer noopener">Join telegram</a>
