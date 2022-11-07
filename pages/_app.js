959cb5db95d6b7a2a6c64deb95593b7fa7ba914b
Petar Popovic <petarpnp@gmail.com>
2022-11-07 21:12:34 +0100

    Comment out wrong google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/*TODO*/}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-NW9VQBBQ61"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}  
-            gtag('js', new Date());
-            gtag('config', 'G-NW9VQBBQ61');
-          `}
-      </Script>
+      {/*<Script*/}
+      {/*  src="https://www.googletagmanager.com/gtag/js?id=G-NW9VQBBQ61"*/}
+      {/*  strategy="afterInteractive"*/}
+      {/*/>*/}
+      {/*<Script id="google-analytics" strategy="afterInteractive">*/}
+      {/*  {`*/}
+      {/*      window.dataLayer = window.dataLayer || [];*/}
+      {/*      function gtag(){dataLayer.push(arguments);}  */}
+      {/*      gtag('js', new Date());*/}
+      {/*      gtag('config', 'G-NW9VQBBQ61');*/}
+      {/*    `}*/}
+      {/*</Script>*/}
       <Component {...pageProps} />
     </>
   )
