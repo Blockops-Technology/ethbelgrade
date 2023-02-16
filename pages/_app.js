ed1a92986426412fc37da0fabd430917cd25e617
Petar Popovic <petarpnp@gmail.com>
2023-02-16 23:52:58 +0100

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,19 +10,18 @@ import "../styles/globals.scss";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/*TODO*/}
-      {/*<Script*/}
-      {/*  src="https://www.googletagmanager.com/gtag/js?id=G-NW9VQBBQ61"*/}
-      {/*  strategy="afterInteractive"*/}
-      {/*/>*/}
-      {/*<Script id="google-analytics" strategy="afterInteractive">*/}
-      {/*  {`*/}
-      {/*      window.dataLayer = window.dataLayer || [];*/}
-      {/*      function gtag(){dataLayer.push(arguments);}  */}
-      {/*      gtag('js', new Date());*/}
-      {/*      gtag('config', 'G-NW9VQBBQ61');*/}
-      {/*    `}*/}
-      {/*</Script>*/}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-9SJ21ZMV21"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}  
+            gtag('js', new Date());
+            gtag('config', 'G-9SJ21ZMV21');
+          `}
+      </Script>
       <style jsx global>{`
         html {
           font-family: ${rubik.style.fontFamily};
