77c6b75c6b5a63d92b821f068fa48b42fd435742
Petar Popovic <petarpnp@gmail.com>
2023-03-01 12:23:03 +0100

    Add team pictures

--- components/landing/team/team.js
+++ components/landing/team/team.js
@@ -10,77 +10,77 @@ const Team = () => (
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/iveza.png" alt="Petar Popovic profile picture" />
+            <img src="/images/team/Petar-Popovic.png" alt="Petar Popovic profile picture" />
             <p className={styles.title}>Petar Popovic</p>
             <p>Founder & Chief Project Manager</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Milos Rakcevic profile picture" />
+            <img src="/images/team/Milos-Rakcevic.png" alt="Milos Rakcevic profile picture" />
             <p className={styles.title}>Milos Rakcevic</p>
             <p>Co-Founder & Head of Partnerships</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Tanja Mladenovic profile picture" />
+            <img src="/images/team/Tanja-Mladenovic.png" alt="Tanja Mladenovic profile picture" />
             <p className={styles.title}>Tanja Mladenovic</p>
             <p>Co-Founder & Head of Marketing</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/iveza.png" alt="Nikola Ivezic profile picture" />
-            <p className={styles.title}>Nikola Ivezic</p>
-            <p>Project Coordinator</p>
-          </a>
-        </div>
-        <div>
-          <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Dusan Markovic profile picture" />
+            <img src="/images/team/Dusan-Markovic.png" alt="Dusan Markovic profile picture" />
             <p className={styles.title}>Dusan Markovic</p>
             <p>Head of Logistics</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Nevena Tomovic profile picture" />
-            <p className={styles.title}>Nevena Tomovic</p>
-            <p>Sales Director</p>
+            <img src="/images/team/Nikola-Ivezic.png" alt="Nikola Ivezic profile picture" />
+            <p className={styles.title}>Nikola Ivezic</p>
+            <p>Project Coordinator</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Natasa Bujosevic profile picture" />
+            <img src="/images/team/Natasa-Bujosevic.png" alt="Natasa Bujosevic profile picture" />
             <p className={styles.title}>Natasa Bujosevic</p>
             <p>Head of Speakers</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Ivan Tabakovic profile picture" />
+            <img src="/images/team/Nevena-Tomovic.png" alt="Nevena Tomovic profile picture" />
+            <p className={styles.title}>Nevena Tomovic</p>
+            <p>Sales Director</p>
+          </a>
+        </div>
+        <div>
+          <a href="" target="_blank" rel="noreferrer noopener">
+            <img src="/images/team/Ivan-Tabakovic.png" alt="Ivan Tabakovic profile picture" />
             <p className={styles.title}>Ivan Tabakovic</p>
             <p>Art Director</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Vasilije Glomazic profile picture" />
+            <img src="/images/team/Vasilije-Glomazic.png" alt="Vasilije Glomazic profile picture" />
             <p className={styles.title}>Vasilije Glomazic</p>
             <p>Content Writer</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Nikola Obradovic profile picture" />
+            <img src="/images/team/Nikola-Obradovic.png" alt="Nikola Obradovic profile picture" />
             <p className={styles.title}>Nikola Obradovic</p>
             <p>Community Lead</p>
           </a>
         </div>
         <div>
           <a href="" target="_blank" rel="noreferrer noopener">
-            <img src="/images/kum.png" alt="Ilija Rilakovic profile picture" />
+            <img src="/images/team/Ilija-Rilakovic.png" alt="Ilija Rilakovic profile picture" />
             <p className={styles.title}>Ilija Rilakovic</p>
             <p>Ilija Rilakovic</p>
           </a>
