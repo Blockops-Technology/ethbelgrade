4cd4e291a41fe3601f390a525455ff58b883ab53
Petar Popovic <petarpnp@gmail.com>
2023-03-19 18:08:11 +0100

    Add prizes section

--- components/hackathon/prizes/prizes.js
+++ components/hackathon/prizes/prizes.js
@@ -1,11 +1,23 @@
 import styles from "./prizes.module.scss";
 
 const Prizes = () => (
-  <div>
+  <div className={styles.prizes}>
     <div className="container">
       <div className={styles.header}>
-        <p className={styles.prizeAmount}>€15,000</p>
+        <p className={styles.totalAmount}>$16,000</p>
         <p className={styles.prizeTitle}>Available in prizes</p>
+      </div>
+      <div>
+        <div className={styles.topRow}>
+          <div className={styles.prize}>
+            <img src="/logo-aligned.svg" alt="ETH Belgrade logo"/>
+            <p className={styles.prizeAmount}>$15,000</p>
+          </div>
+          <div className={styles.prize}>
+            <img src="/images/partners/taikai.svg" alt="TAIKAI logo"/>
+            <p className={styles.prizeAmount}>$1,000</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
