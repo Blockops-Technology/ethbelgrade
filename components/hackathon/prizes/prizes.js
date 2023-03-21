75e61c68c0bff07a2b84a6142cbef1a86110332e
Petar Popovic <petarpnp@gmail.com>
2023-03-21 12:53:37 +0100

    Add Renfter to hackathpn sponsors

--- components/hackathon/prizes/prizes.js
+++ components/hackathon/prizes/prizes.js
@@ -4,7 +4,7 @@ const Prizes = () => (
   <div className={styles.prizes}>
     <div className="container">
       <div className={styles.header}>
-        <p className={styles.totalAmount}>$16,000</p>
+        <p className={styles.totalAmount}>$21,000</p>
         <p className={styles.prizeTitle}>Available in prizes</p>
       </div>
       <div>
@@ -14,8 +14,22 @@ const Prizes = () => (
             <p className={styles.prizeAmount}>$15,000</p>
           </div>
           <div className={styles.prize}>
+            <img src="/images/partners/renfter-logo.svg" alt="Renfter logo"/>
+            <p className={styles.prizeAmount}>$5,000</p>
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.prize}>
             <img src="/images/partners/taikai.svg" alt="TAIKAI logo"/>
-            <p className={styles.prizeAmount}>$1,000</p>
+            <p className={styles.prizeAmount}>$15,000</p>
+          </div>
+          <div className={styles.prize}>
+            {/*<img src="/images/partners/renfter-logo.svg" alt="RENFTER logo"/>*/}
+            <p className={styles.prizeAmount}>Coming soon</p>
+          </div>
+          <div className={styles.prize}>
+            {/*<img src="/images/partners/renfter-logo.svg" alt="RENFTER logo"/>*/}
+            <p className={styles.prizeAmount}>Coming soon</p>
           </div>
         </div>
       </div>
--- components/hackathon/sponsors/sponsors.js
+++ components/hackathon/sponsors/sponsors.js
@@ -7,6 +7,11 @@ const Sponsors = () => (
         <p className={styles.title}>Sponsors</p>
         <div className={styles.sponsorList}>
           <div className={styles.sponsor}>
+            <a href="https://renfter.com/" target="_blank" rel="noreferrer noopener">
+              <img src="/images/partners/renfter-logo.svg" alt="Renfter logo" />
+            </a>
+          </div>
+          <div className={styles.sponsor}>
             <a href="https://taikai.network/" target="_blank" rel="noreferrer noopener">
               <img src="/images/partners/taikai.svg" alt="TAIKAI logo" />
             </a>
