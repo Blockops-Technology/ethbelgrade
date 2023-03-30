5d6e6f56c001b17ce7158de1536dac6cda3ceb8f
Petar Popovic <petarpnp@gmail.com>
2023-03-30 19:51:10 +0200

    Add banner component

---
+++ components/common/banner/banner.js
@@ -0,0 +1,33 @@
+import { useTimer } from "react-timer-hook";
+
+import styles from "./banner.module.scss";
+import Button from "../button/button";
+
+const Banner = () => {
+  const {
+    minutes,
+    hours,
+    days,
+    seconds,
+    isRunning,
+  } = useTimer({ expiryTimestamp: new Date('7 April 2023 15:07:36 GMT+0200 (Central European Summer Time)'), onExpire: () => console.warn('onExpire called') });
+
+
+  return (
+    <div className={styles.banner}>
+      <div className="container">
+        <div className={styles.content}>
+          <div className={styles.textWrapper}>
+            <p className={styles.text}>Tickets price increaseing in</p>
+            <p className={styles.time}>{days} days  {hours} hours  {minutes} minutes {seconds} seconds</p>
+          </div>
+          <a href="https://app.moongate.id/events/ethbelgrade" target="_blank" rel="noreferrer noopener">
+            <Button className={styles.button}>Get Tickets</Button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- components/common/layout/mainLayout.js
+++ components/common/layout/mainLayout.js
@@ -1,9 +1,11 @@
 import Footer from "../footer/footer";
+import Banner from "../banner/banner";
 
 export default function mainLayout(page) {
   return (
     <main>
       {page}
+      <Banner />
       <Footer />
     </main>
   );
