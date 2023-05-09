905d6b5fb8e0a90868f1fc60830a903c35d78acd
Petar Popovic <petarpnp@gmail.com>
2023-05-09 10:34:11 +0200

    Update hackathon page

--- components/hackathon/prizes/prizes.js
+++ components/hackathon/prizes/prizes.js
@@ -4,28 +4,28 @@ const Prizes = () => (
   <div className={styles.prizes}>
     <div className="container">
       <div className={styles.header}>
-        <p className={styles.totalAmount}>$16,000</p>
+        <p className={styles.totalAmount}>$19,000</p>
         <p className={styles.prizeTitle}>Available in prizes</p>
       </div>
       <div>
         <div className={styles.topRow}>
           <div className={styles.prize}>
-            <img src="/logo-aligned.svg" alt="ETH Belgrade logo"/>
+            <img src="/images/partners/EF-ESP-logo.svg" alt="ETH Belgrade logo"/>
             <p className={styles.prizeAmount}>$10,000</p>
           </div>
           <div className={styles.prize}>
-            <img src="/images/partners/renfter-logo.svg" alt="Renfter logo"/>
+            <img src="/logo-aligned.svg" alt="ETH Belgrade logo"/>
             <p className={styles.prizeAmount}>$5,000</p>
           </div>
         </div>
         <div className={styles.row}>
           <div className={styles.prize}>
-            <img src="/images/partners/taikai.svg" alt="TAIKAI logo"/>
-            <p className={styles.prizeAmount}>$1,000</p>
+            <img src="/images/partners/renfter-logo.svg" alt="Renfter logo"/>
+            <p className={styles.prizeAmount}>$3,000</p>
           </div>
           <div className={styles.prize}>
-            {/*<img src="/images/partners/renfter-logo.svg" alt="RENFTER logo"/>*/}
-            <p className={styles.prizeAmount}>Coming soon</p>
+            <img src="/images/partners/taikai.svg" alt="TAIKAI logo"/>
+            <p className={styles.prizeAmount}>$1,000</p>
           </div>
           <div className={styles.prize}>
             {/*<img src="/images/partners/renfter-logo.svg" alt="RENFTER logo"/>*/}
