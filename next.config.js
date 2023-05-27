6ff218790a47d74051c4f1634e5b9aacbf3eb581
Nikola Vukovic <sterlu.sd@gmail.com>
2023-05-27 11:58:12 +0200

    Add redirect for /guide and /agenda for acreditation QR codes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
 
 module.exports = {
   ...nextConfig,
+  async redirects() {
+    return [
+      {
+        source: '/guide',
+        destination: 'https://belgrade-blockchain-week.notion.site/Manual-69d7a82d41fb4eea8ba7e450ec0f12a8',
+        permanent: false,
+      },
+      {
+        source: '/agenda',
+        destination: '/#agenda',
+        permanent: false,
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
