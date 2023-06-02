c57c0dd90c902b43cc55cf7535f6bf0c72433864
Nikola Vukovic <sterlu.sd@gmail.com>
2023-06-02 10:30:41 +0200

    Add map, move agenda up and remove red bar

--- components/landing/hero/hero.js
+++ components/landing/hero/hero.js
@@ -13,26 +13,34 @@ const Hero = () => (
           <img className={styles.label} src="/images/hero-label.png" alt="ETH Belgrade conference label" />
         </div>
       </div>
-      <div className={styles.buttons}>
-        <a href="https://forms.gle/89GkWspMwfCoNyqj9" target="_blank" rel="noreferrer noopener">
-          <Button ghost>
-            Become a partner
-          </Button>
+      <div className={styles.mapWrapper}>
+        <a href="/images/GROUND.png">
+          <img src="/images/GROUND.png" alt="ground floor map" />
         </a>
-        {/*<a href="https://forms.gle/Xf3wV3qp5Jq7oM1W9" target="_blank" rel="noreferrer noopener">*/}
-        {/*  <Button ghost>*/}
-        {/*      Apply as a speaker*/}
-        {/*  </Button>*/}
-        {/*</a>*/}
-        <a href="https://app.moongate.id/events/ethbelgrade" target="_blank" rel="noreferrer noopener">
-          <Button>Get Tickets</Button>
-        </a>
-        <a href="https://taikai.network/ethbelgrade/hackathons/hackathon-2023" target="_blank" rel="noreferrer noopener">
-          <Button ghost>
-            Apply to hack
-          </Button>
+        <a href="/images/UPPER.png">
+          <img src="/images/UPPER.png" alt="upper floor map" />
         </a>
       </div>
+      {/*<div className={styles.buttons}>*/}
+      {/*  /!*<a href="https://forms.gle/89GkWspMwfCoNyqj9" target="_blank" rel="noreferrer noopener">*!/*/}
+      {/*  /!*  <Button ghost>*!/*/}
+      {/*  /!*    Become a partner*!/*/}
+      {/*  /!*  </Button>*!/*/}
+      {/*  /!*</a>*!/*/}
+      {/*  /!*<a href="https://forms.gle/Xf3wV3qp5Jq7oM1W9" target="_blank" rel="noreferrer noopener">*!/*/}
+      {/*  /!*  <Button ghost>*!/*/}
+      {/*  /!*      Apply as a speaker*!/*/}
+      {/*  /!*  </Button>*!/*/}
+      {/*  /!*</a>*!/*/}
+      {/*  /!*<a href="https://app.moongate.id/events/ethbelgrade" target="_blank" rel="noreferrer noopener">*!/*/}
+      {/*  /!*  <Button>Get Tickets</Button>*!/*/}
+      {/*  /!*</a>*!/*/}
+      {/*  /!*<a href="https://taikai.network/ethbelgrade/hackathons/hackathon-2023" target="_blank" rel="noreferrer noopener">*!/*/}
+      {/*  /!*  <Button ghost>*!/*/}
+      {/*  /!*    Apply to hack*!/*/}
+      {/*  /!*  </Button>*!/*/}
+      {/*  /!*</a>*!/*/}
+      {/*</div>*/}
     </div>
   </div>
 );
