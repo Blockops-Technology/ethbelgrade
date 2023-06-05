1be4026d6859cb51516d0d72e4ea7297ffe61e87
Petar Popovic <petarpnp@gmail.com>
2023-06-05 12:01:52 +0200

    Update hackathon page

--- components/hackathon/prizes/prizes.js
+++ components/hackathon/prizes/prizes.js
@@ -4,7 +4,7 @@ const Prizes = () => (
   <div className={styles.prizes}>
     <div className="container">
       <div className={styles.header}>
-        <p className={styles.totalAmount}>$22,000</p>
+        <p className={styles.totalAmount}>$25,000</p>
         <p className={styles.prizeTitle}>Available in prizes</p>
       </div>
       <div>
@@ -20,7 +20,7 @@ const Prizes = () => (
         </div>
         <div className={styles.row}>
           <div className={styles.prize}>
-            <img src="/images/partners/renfter-logo.svg" alt="Renfter logo"/>
+            <img src="/images/hackathon/illuminate.png" alt="Illuminate logo"/>
             <p className={styles.prizeAmount}>$3,000</p>
           </div>
           <div className={styles.prize}>
@@ -28,8 +28,22 @@ const Prizes = () => (
             <p className={styles.prizeAmount}>$3,000</p>
           </div>
           <div className={styles.prize}>
+            <img src="/images/hackathon/EAS.png" alt="EAS logo"/>
+            <p className={styles.prizeAmount}>$3,000</p>
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.prize}>
             <img src="/images/partners/LayerX-logo.svg" alt="Layerx logo"/>
             <p className={styles.prizeAmount}>$1,000</p>
+          </div>
+          <div className={styles.prize}>
+            {/*<img src="/images/partners/MetaMaskSnaps-logo.svg" alt="MetaMask logo"/>*/}
+            {/*<p className={styles.prizeAmount}>$3,000</p>*/}
+          </div>
+          <div className={styles.prize}>
+            {/*<img src="/images/partners/LayerX-logo.svg" alt="Layerx logo"/>*/}
+            {/*<p className={styles.prizeAmount}>$1,000</p>*/}
           </div>
         </div>
       </div>
--- components/hackathon/sponsors/sponsors.js
+++ components/hackathon/sponsors/sponsors.js
@@ -11,16 +11,31 @@ const Sponsors = () => (
               <img src="/images/partners/EF-ESP-logo.svg" alt="EF ESP logo" />
             </a>
           </div>
-          <div className={styles.sponsor}>
-            <a href="https://renfter.com/" target="_blank" rel="noreferrer noopener">
-              <img src="/images/partners/renfter-logo.svg" alt="Renfter logo" />
-            </a>
-          </div>
+          {/*<div className={styles.sponsor}>*/}
+          {/*  <a href="https://renfter.com/" target="_blank" rel="noreferrer noopener">*/}
+          {/*    <img src="/images/partners/renfter-logo.svg" alt="Renfter logo" />*/}
+          {/*  </a>*/}
+          {/*</div>*/}
           <div className={styles.sponsor}>
             <a href="https://metamask.io/" target="_blank" rel="noreferrer noopener">
               <img src="/images/partners/MetaMaskSnaps-logo.svg" alt="MetaMask logo" />
             </a>
           </div>
+          <div className={styles.sponsor}>
+            <a href="https://illuminate.finance/" target="_blank" rel="noreferrer noopener">
+              <img src="/images/hackathon/illuminate.png" alt="Illuminate logo" />
+            </a>
+          </div>
+          <div className={styles.sponsor}>
+            <a href="https://attest.sh/" target="_blank" rel="noreferrer noopener">
+              <img src="/images/hackathon/EAS.png" alt="EAS logo" />
+            </a>
+          </div>
+          {/*<div className={styles.sponsor}>*/}
+          {/*  <a href="https://layerx.xyz/" target="_blank" rel="noreferrer noopener">*/}
+          {/*    <img src="/images/partners/LayerX-logo.svg" alt="LayerX logo" />*/}
+          {/*  </a>*/}
+          {/*</div>*/}
           <div className={styles.sponsor}>
             <a href="https://layerx.xyz/" target="_blank" rel="noreferrer noopener">
               <img src="/images/partners/LayerX-logo.svg" alt="LayerX logo" />
