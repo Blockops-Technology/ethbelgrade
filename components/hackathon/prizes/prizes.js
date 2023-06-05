4d88f3aff7aedfef7d3a95fb5817f32f2173f0e3
Petar Popovic <petarpnp@gmail.com>
2023-06-05 12:13:55 +0200

    Add bounties

--- components/hackathon/prizes/prizes.js
+++ components/hackathon/prizes/prizes.js
@@ -4,7 +4,7 @@ const Prizes = () => (
   <div className={styles.prizes}>
     <div className="container">
       <div className={styles.header}>
-        <p className={styles.totalAmount}>$25,000</p>
+        <p className={styles.totalAmount}>$22,000</p>
         <p className={styles.prizeTitle}>Available in prizes</p>
       </div>
       <div>
@@ -14,8 +14,8 @@ const Prizes = () => (
             <p className={styles.prizeAmount}>$10,000</p>
           </div>
           <div className={styles.prize}>
-            <img src="/logo-aligned.svg" alt="ETH Belgrade logo"/>
-            <p className={styles.prizeAmount}>$5,000</p>
+            <img src="/images/partners/MetaMaskSnaps-logo.svg" alt="MetaMask logo"/>
+            <p className={styles.prizeAmount}>$3,000</p>
           </div>
         </div>
         <div className={styles.row}>
@@ -24,12 +24,12 @@ const Prizes = () => (
             <p className={styles.prizeAmount}>$3,000</p>
           </div>
           <div className={styles.prize}>
-            <img src="/images/partners/MetaMaskSnaps-logo.svg" alt="MetaMask logo"/>
+            <img src="/images/hackathon/EAS.png" alt="EAS logo"/>
             <p className={styles.prizeAmount}>$3,000</p>
           </div>
           <div className={styles.prize}>
-            <img src="/images/hackathon/EAS.png" alt="EAS logo"/>
-            <p className={styles.prizeAmount}>$3,000</p>
+            <img src="/logo-aligned.svg" alt="ETH Belgrade logo"/>
+            <p className={styles.prizeAmount}>$1,000</p>
           </div>
         </div>
         <div className={styles.row}>
