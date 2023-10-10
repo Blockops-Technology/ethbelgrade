a028e9c467eb7e7c794fd73d45f4657c0d379023
Mladen Milankovic <developer@mmlado.com>
2023-10-10 21:10:21 +0200

    Only leave what should work

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const BLOG_URL = 'https://mirror-next-mmlado.vercel.app/'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
@@ -21,53 +20,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-// const BLOG_URL = 'https://mirror-next-mmlado.vercel.app/'
-
-// module.exports = {
-//   // ...nextConfig,
-//   // async redirects() {
-//   //   return [
-//   //     {
-//   //       source: '/guide',
-//   //       destination: 'https://belgrade-blockchain-week.notion.site/Manual-69d7a82d41fb4eea8ba7e450ec0f12a8',
-//   //       permanent: false,
-//   //     },
-//   //     {
-//   //       source: '/agenda',
-//   //       destination: '/#agenda',
-//   //       permanent: false,
-//   //     },
-//   //   ];
-//   // },
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/:path*',
-//         destination: `/:path*`,
-//       },
-//       {
-//         source: '/blog',
-//         destination: `${BLOG_URL}/blog`,
-//       },
-//       {
-//         source: '/blog/:path*',
-//         destination: `${BLOG_URL}/blog/:path*`,
-//       },
-//     ]
-//   },
-//   // webpack(config) {
-//   //   config.module.rules.push({
-//   //     test: /\.svg$/,
-//   //     use: ["@svgr/webpack"]
-//   //   });
-
-//   //   return config;
-//   // }
-// }
