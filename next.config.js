0deef9ca965d4d6ea467bcdd55192cd10c03bdd4
Mladen Milankovic <developer@mmlado.com>
2023-10-10 21:20:54 +0200

    Back to original

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 const BLOG_URL = 'https://mirror-next-mmlado.vercel.app/'
 
-const nextConfig = {
+module.exports = {
+  ...nextConfig,
+  async redirects() {
+    return [
+      {
+        source: '/guide',
+        destination: 'https://belgrade-blockchain-week.notion.site/Manual-69d7a82d41fb4eea8ba7e450ec0f12a8',
+        permanent: false,
+      },
+      {
+        source: '/agenda',
+        destination: '/#agenda',
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
@@ -17,6 +37,12 @@ const nextConfig = {
       },
     ]
   },
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
 
-module.exports = nextConfig
+    return config;
+  }
+}
