f91467b7b20d203966cf7aee88ef2f9cd928cb62
Mladen Milankovic <developer@mmlado.com>
2023-10-10 21:03:00 +0200

    Removing everything except the basics.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
+// const { BLOG_URL } = process.env
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/:path*',
+//         destination: `/:path*`,
+//       },
+//       {
+//         source: '/blog',
+//         destination: `${BLOG_URL}/blog`,
+//       },
+//       {
+//         source: '/blog/:path*',
+//         destination: `${BLOG_URL}/blog/:path*`,
+//       },
+//     ]
+//   },
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -6,21 +31,21 @@ const nextConfig = {
 const BLOG_URL = 'https://mirror-next-mmlado.vercel.app/'
 
 module.exports = {
-  ...nextConfig,
-  async redirects() {
-    return [
-      {
-        source: '/guide',
-        destination: 'https://belgrade-blockchain-week.notion.site/Manual-69d7a82d41fb4eea8ba7e450ec0f12a8',
-        permanent: false,
-      },
-      {
-        source: '/agenda',
-        destination: '/#agenda',
-        permanent: false,
-      },
-    ];
-  },
+  // ...nextConfig,
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/guide',
+  //       destination: 'https://belgrade-blockchain-week.notion.site/Manual-69d7a82d41fb4eea8ba7e450ec0f12a8',
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: '/agenda',
+  //       destination: '/#agenda',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   async rewrites() {
     return [
       {
@@ -37,12 +62,12 @@ module.exports = {
       },
     ]
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"]
+  //   });
 
-    return config;
-  }
+  //   return config;
+  // }
 }
