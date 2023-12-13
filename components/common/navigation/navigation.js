c3ee7ec2331b258233e4beacef2626ef81951959
Petar Popovic <petarpnp@gmail.com>
2023-12-13 18:30:35 +0100

    Add side events to navigation

--- components/common/navigation/navigation.js
+++ components/common/navigation/navigation.js
@@ -5,52 +5,59 @@ import { Menu } from '@headlessui/react'
 import styles from "./navigation.module.scss";
 import HamburgerMenu from "../../../public/icons/hamburger-menu.svg";
 import CloseIcon from "../../../public/icons/x.svg";
+import { BGBW_URL } from "../../../constants";
+import Button from "../button/button";
 
 const MainMenu = () => (
   <>
     <Menu>
-      <Link href="/">
-        <span className={styles.menuItem}>Conference</span>
-      </Link>
-      {/*<Link href="/#speakers">*/}
-      {/*  <span className={styles.menuItem}>Speakers</span>*/}
-      {/*</Link>*/}
-      {/*<Link href="/#agenda">*/}
-      {/*  <span className={styles.menuItem}>Agenda</span>*/}
-      {/*</Link>*/}
-      {/*<Link href="/#partners">*/}
-      {/*  <span className={styles.menuItem}>Partners</span>*/}
-      {/*</Link>*/}
-      {/*<Link href="/#startups">*/}
-      {/*  <span className={styles.menuItem}>Startups</span>*/}
-      {/*</Link>*/}
-      <Link href="/hackathon">
-        <span className={styles.menuItem}>Hackathon</span>
-      </Link>
-      <Link href="/community">
-        <span className={styles.menuItem}>Community</span>
-      </Link>
-      <div className={styles.dropdownWrapper}>
-        <Menu.Button className={styles.menuItem}>Past Events </Menu.Button>
-        <Menu.Items className={styles.dropdown}>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                href="https://2023.ethbelgrade.rs"
-                target="_blank"
-                rel="noreferrer noopener"
-                className={styles.dropdownLink}
-              >
-                <img src="/icons/play-arrow.svg" alt="arrow icon" />
-                2023
-              </Link>
-            )}
-          </Menu.Item>
-        </Menu.Items>
+      <div className={styles.menuLinks}>
+        <Link href="/">
+          <span className={styles.menuItem}>Conference</span>
+        </Link>
+        {/*<Link href="/#speakers">*/}
+        {/*  <span className={styles.menuItem}>Speakers</span>*/}
+        {/*</Link>*/}
+        {/*<Link href="/#agenda">*/}
+        {/*  <span className={styles.menuItem}>Agenda</span>*/}
+        {/*</Link>*/}
+        {/*<Link href="/#partners">*/}
+        {/*  <span className={styles.menuItem}>Partners</span>*/}
+        {/*</Link>*/}
+        {/*<Link href="/#startups">*/}
+        {/*  <span className={styles.menuItem}>Startups</span>*/}
+        {/*</Link>*/}
+        <Link href="/hackathon">
+          <span className={styles.menuItem}>Hackathon</span>
+        </Link>
+        <Link href="/community">
+          <span className={styles.menuItem}>Community</span>
+        </Link>
+        <div className={styles.dropdownWrapper}>
+          <Menu.Button className={styles.menuItem}>Past Events </Menu.Button>
+          <Menu.Items className={styles.dropdown}>
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="https://2023.ethbelgrade.rs"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className={styles.dropdownLink}
+                >
+                  <img src="/icons/play-arrow.svg" alt="arrow icon" />
+                  2023
+                </Link>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </div>
+        <a href={BGBW_URL} target="_blank" rel="noreferrer noopener">
+          <Button className={styles.button}>Side Events</Button>
+        </a>
+        {/*<Link href="#faq">*/}
+        {/*  <span className={styles.menuItem}>FAQ</span>*/}
+        {/*</Link>*/}
       </div>
-      {/*<Link href="#faq">*/}
-      {/*  <span className={styles.menuItem}>FAQ</span>*/}
-      {/*</Link>*/}
     </Menu>
   </>
 );
