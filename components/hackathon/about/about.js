08de85c305567bb97e8fae68e2f83f6caa621b45
Petar Popovic <petarpnp@gmail.com>
2024-02-13 14:21:33 +0100

    Update hackathon page

--- components/hackathon/about/about.js
+++ components/hackathon/about/about.js
@@ -1,13 +1,14 @@
 import styles from "./about.module.scss";
 import Button from "../../common/button/button";
+import { HACKATHON_APPLICATION_FORM_URL } from "../../../constants";
 
 const About = () => (
   <div id="about" className={styles.about}>
-    {/*<div className="container">*/}
-    {/*  <div className={styles.deadlineContainer}>*/}
-    {/*    <p className={styles.deadline}>Registration will close on May 26, 2023</p>*/}
-    {/*  </div>*/}
-    {/*</div>*/}
+    <div className="container">
+     <div className={styles.deadlineContainer}>
+       <p className={styles.deadline}>Registration will close on May 20, 2024</p>
+     </div>
+    </div>
     <div className={styles.contentWrapper}>
       <div className="container">
         <div className={styles.content}>
@@ -15,16 +16,21 @@ const About = () => (
           <div className={styles.aboutContent}>
             <p className={styles.sectionTitle}>About the hackathon</p>
             <p className={styles.sectionText}>
-              ETH Belgrade Hackathon is a three-day competition that gathers talents from all around the world. Our mission is to empower engineers to push the limits of new technologies and build innovative real-world use cases on the Ethereum blockchain.
-              <br/>
-              <br/>
-              {/*To find detailed information about ETH Belgrade Hackathon, please visit our official TAIKAI page.*/}
+              ETH Belgrade Hackathon offers a three-day hacking experience with a primary focus on building on Ethereum. Following the success of the inaugural hackathon in 2023, we aim to surpass it in every aspect.
+              <br />
+              <br />
+              This year, we're opening the doors for builders from all around the globe to join us both in-person and online (hybrid). Our goal is to gather hundreds of hackers and curious minds to develop projects and use cases that will reshape the Ethereum landscape and enhance people's lives.
+              <br />
+              <br />
+              An attractive prize pool, numerous bounties, collaborations with people from top-tier protocols and an unparalleled opportunity to bring your fresh ideas to life—these are just some of the benefits you'll gain by participating in the second edition of the ETH Belgrade conference.
             </p>
-            {/*<a href="https://taikai.network/ethbelgrade/hackathons/hackathon-2023" target="_blank" rel="noreferrer noopener">*/}
-            {/*  <Button className={styles.button}>*/}
-            {/*    See the guide*/}
-            {/*  </Button>*/}
-            {/*</a>*/}
+            <p className={styles.sectionText}>To find detailed information about ETH Belgrade Hackathon, please visit our official TAIKAI page.</p>
+            
+            <a href={HACKATHON_APPLICATION_FORM_URL} target="_blank" rel="noreferrer noopener">
+              <Button className={styles.button}>
+                See the guide
+              </Button>
+            </a>
           </div>
         </div>
       </div>
--- constants.js
+++ constants.js
@@ -3,3 +3,4 @@ export const SPEAKER_APPLICATION_FORM_URL = "https://docs.google.com/forms/d/e/1
 export const SPONSOR_APPLICATION_FORM_URL = "https://forms.gle/noyd3TYynSh1yuPv8";
 export const TICKETING_PLATFORM_URL = "https://app.moongate.id/e/ethbelgrade2024";
 export const BGBW_URL = "https://belgradeblockchainweek.com/"
+export const HACKATHON_APPLICATION_FORM_URL = "https://taikai.network/ethbelgrade/hackathons/eth-belgrade-hackathon-2024";
