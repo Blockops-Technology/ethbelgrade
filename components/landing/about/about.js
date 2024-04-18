7441dd6703ef1fe30439161531fd13a96eee9810
Mladen Milankovic <developer@mmlado.com>
2024-04-18 05:53:39 +0200

    About boxes formatted.

--- components/landing/about/about.js
+++ components/landing/about/about.js
@@ -20,17 +20,17 @@ const About = () => (
       <div className="container">
         <div className={styles.content}>
           <div className={styles.belgradeCalling}>
-            <p className={styles.sectionTitle}>THE MOST WELCOMING CONFERENCE</p>
-            <p className={styles.sectionText}>For builders. For Web3 enthusiasts. For Ethereum believers. ETH Belgrade extends a warm embrace to all creatives and explorers eager to share knowledge that will ignite the entire ecosystem. Both the conference and hackathon are crafted to connect like-minded people, providing opportunities for their wildest ideas to come to life. Let&apos;s build together!</p>
+            <p className={styles.sectionTitle}>ETH Belgrade CALLING</p>
+            <p className={styles.sectionText}>ETH Belgrade is a playground for exploring Ethereum possibilities. As part of Belgrade Blockchain Week, this three-day conference gathers extraordinary minds and Ethereum enthusiasts to share knowledge and spark ideas that will ignite the whole ecosystem. And what’s best, as soon as the conference ends, the “after party” begins - the ETH Belgrade Hackathon, which will gather brilliant minds in three days of non-stop action.</p>
           </div>
           <div className={styles.whyBelgrade}>
             <p className={styles.sectionTitle}>Why Belgrade?</p>
             <p className={styles.sectionText}>The past and future have been flirting in Belgrade for centuries. Located where the Danube hugs Sava and the East clashes with the West, the city blends different cultures to create eclectic charm. Authentic, vibrant, and sometimes chaotic, Belgrade is built upon tradition, cosmopolitan spirit, and booming technologies.</p>
             <p className={styles.sectionText}>Belgrade is home to 50+ Web3 companies creating a fruitful and fast-expanding local community. Many OGs, buidlers, and heavyweights like <strong>Polygon</strong>, <strong>Tenderly</strong>, and <strong>DeFi Saver</strong>, were born and raised in the city. But — one thing is for sure — they are not the last Web3 Belgraders you’ll hear about.</p>
-            <a href="https://belgradeblockchainweek.com/discover-belgrade" target="_blank" rel="noreferrer noopener">
-              <Button className={styles.button}>Discover belgrade</Button>
-            </a>
           </div>
+          <a href="https://belgradeblockchainweek.com/discover-belgrade" target="_blank" rel="noreferrer noopener">
+              <Button className={styles.button}>Discover belgrade</Button>
+          </a>
         </div>
       </div>
     </div>
