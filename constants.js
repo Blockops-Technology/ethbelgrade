15fad3c86b5856ef1ed9874910a997b0eb7438e6
Petar Popovic <petarpnp@gmail.com>
2024-05-13 18:29:33 +0200

    Update prize pool

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 export const YEAR = 2024;
-export const HACKATHON_PRIZE_AMOUNT = "25.000";
+export const HACKATHON_PRIZE_AMOUNT = "40.000";
 export const SPEAKER_APPLICATION_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdG5twtn_xCLvEDYzFMw4Af48kK0oE9_FNTsu_3fHZLVvMH7Q/viewform?pli=1";
 export const SPONSOR_APPLICATION_FORM_URL = "https://forms.gle/noyd3TYynSh1yuPv8";
 export const TICKETING_PLATFORM_URL = "https://app.moongate.id/e/ethbelgrade2024";
