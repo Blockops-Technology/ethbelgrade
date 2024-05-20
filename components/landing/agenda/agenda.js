16bb2fe8809419fe9527903fabf9abab7325ebb9
Mladen Milankovic <developer@mmlado.com>
2024-05-20 06:01:06 +0200

    Add timeslots

--- components/landing/agenda/agenda.js
+++ components/landing/agenda/agenda.js
@@ -25,6 +25,34 @@ const SpeakerList = (props) => {
   });
 }
 
+const timeslots = [
+  "9:00",
+  "9:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+  "19:30",
+  "20:00",
+  "20:30",
+  "21:00",
+];
+
 const Agenda = () => (
   <div id="agenda" className={styles.agenda}>
     <div className="container">
@@ -49,6 +77,24 @@ const Agenda = () => (
           {
             agenda.days.map((day, i) => (
               <TabPanel key={i}>
+                <div>
+                  <div>
+                    <div></div>
+                    <div>Stage 1</div>
+                    <div>Stage 2</div>
+                    <div>Stage 3</div>
+                  </div>
+                {
+                  timeslots.map((timeslot, i) => {
+                    <div>
+                      <div>{timeslot}</div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                  })
+                }
+                </div>
                 {/* {
                   stage.programme.map((day, i) => (
                     <div key={i}>
