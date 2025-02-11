89b17a4a6c6d0341b1997d6bb84470916c352804
Mladen Milankovic <developer@mmlado.com>
2025-02-11 06:38:35 +0100

    WIP: Aranging talks.

--- components/landing/agenda/agenda.js
+++ components/landing/agenda/agenda.js
@@ -26,45 +26,51 @@ const SpeakerList = (props) => {
 }
 
 function Detail({ item }) {
-  // const style = {
-  //   "--duration": item.slots ? item.slots : 1,
-  //   "--spaces": item.spaces ? item.spaces : 1,
-  // };
+  if (item === undefined) {
+    return (
+      <div className={styles.details}></div>
+    )
+  }
+  const style = {
+    "--duration": item.slots ? item.slots : 1,
+    "--spaces": item.spaces ? item.spaces : 1,
+  };
 
-  // const classes = item.category?.toLowerCase()
-  //   .replaceAll(" ", "")
-  //   .replaceAll("/", "")
-  //   .split(",")
-  //   .map(cat => styles["talkCategory" + cat])
-  //   .join(' ');
+  const classes = item.category?.toLowerCase()
+    .replaceAll(" ", "")
+    .replaceAll("/", "")
+    .split(",")
+    .map(cat => styles["talkCategory" + cat])
+    .join(' ');
 
-  // if (item.title || item.speaker || item.type)
-  //   return (
-  //     <div className={styles.details} style={style}>
-  //       <div className={`${styles.talk} ${classes}`}>
-  //         {item.category.replaceAll("Break", "") && item.category.replaceAll("Break", "").split(', ').map(cat => (<div key={cat} className={styles.talkType}>{cat}</div>))}
-  //         <div className={styles.talkTitle}>{item.title ? item.title : "TBA"}</div>
-  //         {item.speaker && (
-  //           <div className={styles.talkSpeaker}>
-  //             <SpeakerList name={`${item.speaker}`} />
-  //             {item.company && <span>, {item.company}</span>}
-  //           </div>
-  //         )}
-  //         {
-  //           item.youtube &&
-  //           <div>
-  //             <Link href={`${item.youtube}`} target="_blank" rel="noreferrer noopener">
-  //               <Button className={styles.viewOnYt}>
-  //                 <img className={styles.playIcon} src="/icons/play.svg" alt="Play icon" />
-  //                 Watch on YouTube
-  //               </Button>
-  //             </Link>
-  //           </div>
-  //         }
-  //       </div>
-  //     </div>
-  //   )
-  // else
+  console.log(item)
+  if (item.slug != "empty" || item.speakers.length)
+    return (
+      <div className={styles.details} style={style}>
+        <div className={`${styles.talk} ${classes}`}>
+          {/* {item.category.replaceAll("Break", "") && item.category.replaceAll("Break", "").split(', ').map(cat => (<div key={cat} className={styles.talkType}>{cat}</div>))} */}
+          <div className={styles.talkTitle}>{item.title ? item.title : "TBA"}</div>
+          {item.speaker && (
+            <div className={styles.talkSpeaker}>
+              <SpeakerList name={`${item.speaker}`} />
+              {item.company && <span>, {item.company}</span>}
+            </div>
+          )}
+          {
+            item.youtube &&
+            <div>
+              <Link href={`${item.youtube}`} target="_blank" rel="noreferrer noopener">
+                <Button className={styles.viewOnYt}>
+                  <img className={styles.playIcon} src="/icons/play.svg" alt="Play icon" />
+                  Watch on YouTube
+                </Button>
+              </Link>
+            </div>
+          }
+        </div>
+      </div>
+    )
+  else
     return (
       <div className={styles.details}></div>
     )
@@ -83,16 +89,13 @@ function TimeSlot({ time, programe }) {
   )
 }
 
-const startTime = 10;
+const startTime = 9;
 const endTime = 17;
 const timeSlots = [
   "08:30",
-  "09:00",
-  "09:30"
 ];
 
 for (let hour = startTime; hour < endTime; hour++) {
-  // Adding two slots per hour: XX:00 and XX:30
   timeSlots.push(`${hour}:00`);
   timeSlots.push(`${hour}:30`);
 }
@@ -102,6 +105,7 @@ timeSlots.push("17:00")
 const Agenda = ({ agenda }) => {
   let days = {};
   let spaces = {};
+  let talks = {};
   agenda.map((day_and_space, i) => {
     const data = day_and_space.meta.en
     const day = data.day.meta.en
@@ -111,20 +115,38 @@ const Agenda = ({ agenda }) => {
       const date = new Date(day.date.timestamp)
       days[day.title] = {
         dayName: day.title,
-        date: date.toLocaleDateString('en-US', { day: 'numeric', month: 'long' })
+        slug: day.slug,
+        date: date.toLocaleDateString('en-US', { day: 'numeric', month: 'long' }),
+        programe: {}
       }
     }
 
     if (!(space.title in spaces)) {
-      spaces[space.title] = {
-        name: space.title,
-        pronounciation: space.pronounciation,
-        location: space.location
-      }
+      spaces[space.title] = space
     }
+
+    data.talks.map((talkData, i) => {
+      console.log(talkData)
+      if (!(day.slug in talks))
+        talks[day.slug] = {}
+      talks[day.slug][space.slug] = talkData.meta.en
+
+    })
   });
   days = Object.values(days)
   spaces = Object.values(spaces)
+  console.log(talks)
+  days.map((day, i) => {
+    var timeSlotIndex = 0
+    spaces.map((space, i) => {
+      const timeSlot = timeSlots[timeSlotIndex]
+      if (!(timeSlot in day.programe))
+        day.programe[timeSlot] = []
+      day.programe[timeSlot].push(talks[day.slug][space.slug])
+    })
+    ++timeSlotIndex
+  })
+  console.log(days[0].programe)
 
   return <div id="agenda" className={styles.agenda}>
     <div className="container no-padding">
@@ -157,12 +179,11 @@ const Agenda = ({ agenda }) => {
                         <h3>{space.name} <span>[{space.pronounciation}]</span></h3>
                         <h4>{space.location}</h4>
                       </div>
-
                     ))
                   }
                   {
                     timeSlots.map((time, index) => (
-                      <TimeSlot key={time} programe={[null, null, null, null]} time={time} index={index} start={1} duration={1} />
+                      <TimeSlot key={time} programe={day.programe[time]} time={time} index={index} start={1} duration={1} />
                     ))
                   }
                 </div>
