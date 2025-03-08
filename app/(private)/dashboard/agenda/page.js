0810afc1f0f485bf1868af3015052dd9e70c4309
Petar Popovic <petarpnp@gmail.com>
2025-03-08 21:25:02 +0100

    Agenda dash page start

--- app/(private)/dashboard/agenda/page.js
+++ app/(private)/dashboard/agenda/page.js
@@ -1,5 +1,20 @@
 const Agenda = () => {
-  return <div>Agenda</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Agenda</h1>
+
+        <div className="bg-[var(--primary-blue)] hover:bg-[var(--primary-blue)]/70 hover:text-white cursor-pointer py-2 px-4 rounded-sm">Add Day</div>
+
+      </div>
+      <div className="mt-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-bold">Day 1</h2>
+          <div className="bg-[var(--primary-blue)] hover:bg-[var(--primary-blue)]/70 hover:text-white cursor-pointer py-2 px-4 rounded-sm">Add Schedule</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Agenda;
