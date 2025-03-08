268c99d49be57ebf8f14aa74358cad4a18c84f36
Petar Popovic <petarpnp@gmail.com>
2025-03-08 21:28:15 +0100

    Add sign in page

---
+++ app/sign-in/page.js
@@ -0,0 +1,13 @@
+const SignIn = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="max-w-sm">
+          <button className="bg-[var(--primary-blue)] hover:bg-[var(--primary-blue)]/70 hover:text-white cursor-pointer py-2 px-4 rounded-sm w-full">Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
