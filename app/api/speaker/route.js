feecdfcb4861af1c3c382aa53c2bdd1d1e75b7fd
Mladen Milankovic <developer@mmlado.com>
2025-04-24 06:13:43 +0200

    Add order field.

--- app/api/speaker/route.js
+++ app/api/speaker/route.js
@@ -2,11 +2,13 @@ import connectMongo from "@/lib/mongoose";
 import Speaker from "@/models/Speaker";
 import { NextResponse } from "next/server";
 
+const ORDER_SPACING = 1000000;
+
 export async function GET(request) {
   await connectMongo();
 
   try {
-    const speakers = await Speaker.find();
+    const speakers = await Speaker.find().sort({order: 1});
     console.log(speakers);
     return NextResponse.json(speakers);
   } catch (error) {
@@ -20,6 +22,7 @@ export async function POST(request) {
 
   try {
     const body = await request.json();
+    body.order = await Speaker.find({}).sort({order: -1}).limit(1).then(speaker => speaker && speaker.length ? speaker[0].order : 0) + ORDER_SPACING;
     const speaker = await Speaker.create(body);
     return NextResponse.json(speaker, { status: 201 });
   } catch (error) {
--- models/Speaker.js
+++ models/Speaker.js
@@ -21,6 +21,11 @@ const speakerSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    order: {
+      type: Number,
+      required: true,
+      default: 0,
+    }
   },
   {
     timestamps: true,
